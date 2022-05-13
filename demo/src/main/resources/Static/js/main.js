0aeb3203c6903d8824aeff335d546c49486d6c34
Kevin Villagra <kevinvillagra24@gmail.com>
2022-05-12 21:51:29 -0300

    Correcion de errores de la vista index

--- demo/src/main/resources/Static/js/main.js
+++ demo/src/main/resources/Static/js/main.js
@@ -1,20 +1,19 @@
-jQuery(function(){
-    $("#btn").on("click", function(){
-        if($("#ct-publicacion").is(" :visible")){
-            $("#ct-publicacion").hide("slow");
-            $("#ct-publicaciones").hide("slow");
-            $("#ct-trabajo").show("slow");
-            $("#ct-trabajos").show("slow");
-            $("#btn").text("Publicaciones");
-        }else{
-            $("#ct-trabajo").hide("slow");
-            $("#ct-trabajos").hide("slow");
-            $("#ct-publicacion").show("slow");
-            $("#ct-publicaciones").show("slow");
-            $("#btn").text("Trabajos");
-        }
-    })
-   
-        
-    
-})
+    function toogle() {
+    var element = document.getElementById('ct-trabajo');
+    var element2 = document.getElementById('ct-publicacion');
+    var element3 = document.getElementById('ct-trabajos');
+    var element4 = document.getElementById('ct-publicaciones');
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+    element3.style.display = 'block';
+    element2.style.display = 'none';
+    element4.style.display = 'none';
+    document.getElementById('btn').innerText = 'Publicacion';
+  } else {
+    element.style.display = 'none';
+    element3.style.display = 'none';
+    element2.style.display = 'block';
+    element4.style.display = 'block';
+    document.getElementById('btn').innerText = 'Trabajo';
+  }
+}
